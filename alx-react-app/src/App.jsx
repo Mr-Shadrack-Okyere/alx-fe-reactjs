a9bbded9dc16f177c02e11d05e351e4f889d1861
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 23:58:34 +0000

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -25,3 +25,16 @@ function App() {
 }
 
 export default App;
+
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="Bob" age="30" bio="Enjoys cooking and traveling" />
+    </div>
+  );
+}
+
+export default App;
