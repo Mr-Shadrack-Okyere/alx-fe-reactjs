202b3ea0b2f670729ff90729d1f184008e317e08
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-24 23:15:19 +0000

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,38 @@
+import { Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+
+        <Route
+          path="/recipes/:id"
+          element={<RecipeDetailsWrapper />}
+        />
+      </Routes>
     </div>
   );
 }
+
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={id} />;
+};
+
+import { useParams } from 'react-router-dom';
 
 export default App;
