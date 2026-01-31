fd41e749b5c87fa0c9a547326f1dff579c8b9bdd
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-31 23:27:44 +0000

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -27,3 +27,17 @@ export const fetchAdvancedUsers = async (username, location, minRepos, page = 1)
 
   return response.data;
 };
+
+import axios from "axios";
+
+export const fetchAdvancedUsers = async (username, location, minRepos, page = 1) => {
+  let query = username;
+
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
+
+  const url = `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`;
+
+  const response = await axios.get(url);
+  return response.data;
+};
