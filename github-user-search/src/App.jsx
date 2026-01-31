7583eb19de066677a78690903454b01be1582264
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-31 23:17:22 +0000

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -10,3 +10,16 @@ function App() {
 }
 
 export default App;
+
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
