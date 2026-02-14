c2cee8ae8831d458a10ff48bcb16c90e1ec14ba7
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-14 22:45:29 +0000

    Create HomePage.jsx

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+<div className="text-center mb-6">
+  <Link
+    to="/add-recipe"
+    className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+  >
+    Add New Recipe
+  </Link>
+</div>
