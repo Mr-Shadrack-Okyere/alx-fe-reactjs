65075f9339d86bc5310bfde36deb94196d34b6eb
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-14 22:55:31 +0000

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
