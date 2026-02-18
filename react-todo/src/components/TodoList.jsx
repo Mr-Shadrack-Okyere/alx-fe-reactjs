40eaa674d4d3787a28a03b732e8156d1990301cb
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-18 01:00:33 +0000

    Update TodoList.jsx

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -74,3 +74,53 @@ export default function TodoList() {
     </div>
   );
 }
+
+
+import { useState } from "react";
+import AddTodoForm from "./AddTodoForm";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Learn Jest", completed: false }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <AddTodoForm addTodo={addTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            onClick={() => toggleTodo(todo.id)}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none",
+              cursor: "pointer"
+            }}
+          >
+            {todo.text}{" "}
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
